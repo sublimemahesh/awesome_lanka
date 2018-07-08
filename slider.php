cb4f32c213c2ca4a2ae56264d68eb1a6b4afc016
chalanadulaj <chalanadulaj99@gmail.com>
2018-07-08 15:09:39 +0530

    #9 add slider php in admin panel

--- slider.php
+++ slider.php
@@ -3,66 +3,28 @@
     <div class="row">     
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-bottom: 26px;">
 
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
-
             <div class="carousel-inner">
-                <div class="item active">
-                    <img src="assets/images/slide6.jpg" alt="First slide" height="200px">
-                    <!--                     <div class="col-md-12 text-center">
-                                                <h2>
-                                                    <span>Welcome to <strong>LOREM IPSUM</strong></span>
-                                                </h2>
-                                                <br>
-                                                <h3>
-                                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                                                </h3>
-                                                <br>
-                                                <div class="">
-                                                  </div>
-                                            </div>-->
-                    <div class="header-text hidden-xs">
+                <?php
+                $li = '';
+                foreach (Slider::all() as $key => $Simages) {
+                    if ($key === 0) {
+                        $li .= ' <li data-target="#carousel-example-generic" data-slide-to="' . $key . '" class="active"></li>';
+                        ?>
+                        <div class="item active">
+                            <img src="upload/slider/<?php echo $Simages['image_name']; ?>" alt="First slide" >
+                        </div>
+                        <?php
+                    } else {
+                        $li .= '<li data-target="#carousel-example-generic" data-slide-to="' . $key . '"></li>';
+                        ?>
+                        <div class="item">
+                            <img src="upload/slider/<?php echo $Simages['image_name']; ?>" alt="First slide" >
 
-                    </div> 
-                </div>
-                <div class="item">
-                    <img src="assets/images/slide2.jpg" alt="First slide" height="200px">
-
-                    <!--                    <div class="header-text hidden-xs">
-                                            <div class="col-md-12 text-center">
-                                                <h2>
-                                                    <span>Welcome to LOREM IPSUM</span>
-                                                </h2>
-                                                <br>
-                                                <h3>
-                                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                                                </h3>
-                                                <br>
-                                                <div class="">
-                                                </div>
-                                            </div>
-                                        </div> -->
-                </div>
-                <div class="item">
-                    <img src="assets/images/slide3.jpg" alt="First slide" height="200px">
-                    <!--                    <div class="header-text hidden-xs">
-                                            <div class="col-md-12 text-center">
-                                                <h2>
-                                                    <span>Welcome to LOREM IPSUM</span>
-                                                </h2>
-                                                <br>
-                                                <h3>
-                                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                                                </h3>
-                                                <br>
-                                                <div class="">
-                                                </div>
-                                            </div> 
-                                        </div>-->
-                </div>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
                 <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left"></span>
                 </a>
@@ -70,6 +32,9 @@
                     <span class="glyphicon glyphicon-chevron-right"></span>
                 </a>
             </div> 
+            <ol class="carousel-indicators"> 
+                <?php echo $li; ?>
+            </ol>
         </div>
     </div>
 </div>
