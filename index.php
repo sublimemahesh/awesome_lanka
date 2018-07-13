11b4b090480ea9321d6231c3580efe2927b8892c
mgashini95 <mganisansala@gmail.com>
2018-07-13 16:49:30 +0530

    #54 Edit index

--- index.php
+++ index.php
@@ -28,7 +28,11 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="panel">
+<<<<<<< Updated upstream
                             <div class="panel-body" style="padding-bottom: 1px;">
+=======
+                            <div class="panel-body" style="padding-bottom: 0px;">
+>>>>>>> Stashed changes
                                 <div class="tab-content">
                                     <div class="tab-pane fade in active" id="tab1default">
                                         <div class="row">
@@ -335,6 +339,7 @@
 
             <!-- blog section -->
 
+<<<<<<< Updated upstream
             <div class="reference home-ref">
                 <div class="container">
                     <div class="row">
@@ -375,6 +380,46 @@
                                             neque nisi placeat possimus quaerat quam repudiandae sint.
                                             <span class="author">- Bob Smith, Fitbit</span>
                                         </blockquote>
+=======
+
+                <div class="reference home-refr">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-3">
+                                <div class="title">
+                                    <h2>References</h2>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="testimonials">
+                                <div class="carousel" data-ride="carousel" id="quote-carousel">
+                                    <?php
+                                    foreach (Comments::activeComments() as $key => $comment) {
+                                        ?>
+                                        <div class="carousel-inner">
+                                            <!-- Quote 1 -->  
+                                            <div class="item col-sm-10 col-sm-offset-1">
+                                                <blockquote>
+                                                  <?php echo $comment['comment'] ?>
+                                                        <span class="author"><?php echo $comment['name'] ?></span>
+                                                </blockquote>
+                                            </div>
+                                            <!-- Quote 2 -->  
+
+                                            <!-- Quote 3 -->
+
+                                        </div>
+                                        <!-- Bottom Carousel Indicators -->
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#quote-carousel" data-slide-to="0" class=""><img class="img-responsive " src="assets/images/avtar-1.jpg" alt=""></li>
+                                            <li data-target="#quote-carousel" data-slide-to="1" class=""><img class="img-responsive" src="assets/images/avtar-2.jpg" alt=""></li>
+                                            <li data-target="#quote-carousel" data-slide-to="2" class="active"><img class="img-responsive" src="assets/images/avtar-3.jpg" alt=""></li>
+                                        </ol>
+                                    <?php }
+                                        ?>
+>>>>>>> Stashed changes
                                     </div>
                                 </div>
                                 <!-- Bottom Carousel Indicators -->
@@ -386,6 +431,7 @@
                             </div>
                         </div>
                     </div>
+<<<<<<< Updated upstream
                 </div>
             </div>
             <!-- Newsletter -->
@@ -416,6 +462,36 @@
         <script src="assets/js/jquery.easing.min.js" type="text/javascript"></script>
         <!-- custom -->
         <script src="assets/js/custom.js" type="text/javascript"></script>
+=======
+                    <!-- Newsletter -->
+
+                </div>
+                <!-- Footer Section -->
+                <?php include './footer.php'; ?>
+            <!-- jQuery -->
+            <script data-cfasync="false" src="../cdn-cgi/scripts/f2bf09f8/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js" type="text/javascript"></script>
+            <!-- jquery ui js -->
+            <script src="assets/js/jquery-ui.min.js" type="text/javascript"></script>
+            <!-- bootstrap js -->
+            <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+            <!-- fraction slider js -->
+            <script src="assets/js/jquery.fractionslider.js" type="text/javascript"></script>
+            <!-- owl carousel js --> 
+            <script src="assets/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+            <!-- counter -->
+            <script src="assets/js/jquery.counterup.min.js" type="text/javascript"></script>
+            <script src="assets/js/waypoints.js" type="text/javascript"></script>
+            <!-- filter portfolio -->
+            <script src="assets/js/jquery.shuffle.min.js" type="text/javascript"></script>
+            <script src="assets/js/portfolio.js" type="text/javascript"></script>
+            <!-- magnific popup -->
+            <script src="assets/js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+            <!-- range slider -->
+            <script src="assets/js/ion.rangeSlider.min.js" type="text/javascript"></script>
+            <script src="assets/js/jquery.easing.min.js" type="text/javascript"></script>
+            <!-- custom -->
+            <script src="assets/js/custom.js" type="text/javascript"></script>
+>>>>>>> Stashed changes
     </body>
 
 </html>
